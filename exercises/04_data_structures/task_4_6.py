e69f5b3cc0d48e011183eb6619099aa088a0b9c5
jewdas1984 <alexatwork0@gmail.com>
2018-07-10 10:17:17 -0400

    Solved

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -15,3 +15,22 @@ Outbound Interface:    FastEthernet0/0
 '''
 
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+
+XYZ = (ospf_route.split())
+XYZ[0] = 'OSPF'
+XYZ[2] = XYZ[2].replace(']','').replace('[','')
+XYZ[5] = XYZ[5].replace(',','')
+XYZ[4] = XYZ[4].replace(',','')
+print(XYZ)
+
+zaloopa = '''
+
+Protocol:              {d[0]}
+Prefix:                {d[1]}
+AD/Metric:             {d[2]}
+Next-Hop:              {d[4]}
+Last update:           {d[5]}
+Outbound Interface:    {d[6]}
+
+'''
+print(zaloopa.format(d=XYZ))
