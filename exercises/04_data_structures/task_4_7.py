bc9e304371de9289210816af780714db4b847e5e
jewdas1984 <alexatwork0@gmail.com>
2018-07-11 17:36:05 -0400

    Solved

--- exercises/04_data_structures/task_4_7.py
+++ exercises/04_data_structures/task_4_7.py
@@ -9,3 +9,11 @@
 '''
 
 MAC = 'AAAA:BBBB:CCCC'
+MAC_list = MAC.replace(':','')
+MAC_list_1 = MAC_list[0:2]
+MAC_list_2 = MAC_list[2:4]
+X1 = str(int(MAC_list_1, 16))
+X2 = str(int(MAC_list_2, 16))
+print(X1,X2)
+result_x = str(X1 + X2)
+print(result_x)
