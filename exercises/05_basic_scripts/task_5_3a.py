62c602e3406315b89f6c8bb47c03faedfae35767
jewdas1984 <alexatwork0@gmail.com>
2018-07-14 09:36:21 -0400

    Solved

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -11,13 +11,27 @@
 То есть эту задачу можно решить без использования условия if и циклов for/while.
 '''
 
-access_template = [
-    'switchport mode access', 'switchport access vlan {}',
-    'switchport nonegotiate', 'spanning-tree portfast',
-    'spanning-tree bpduguard enable'
-]
+access_template = '''
+    interface {}
+    switchport mode access
+    switchport access vlan {}
+    switchport nonegotiate
+    spanning-tree portfast
+    spanning-tree bpduguard enable
+'''
 
-trunk_template = [
-    'switchport trunk encapsulation dot1q', 'switchport mode trunk',
-    'switchport trunk allowed vlan {}'
-]
+trunk_template = '''
+    interface {} 
+    switchport trunk encapsulation dot1q
+    switchport mode trunk
+    switchport trunk allowed vlan {}
+'''
+
+dict ={'access' : {'Interface': 'None', 'Vlan': 'None', 'Template': access_template, 'Question': 'Enter VLAN number: '}, 
+'trunk' : {'Interface': 'None', 'Vlan': 'None', 'Template': trunk_template, 'Question': 'Enter Allowed VLANs: '}}
+i1 = input('Enter interface mode (access/trunk): ')
+i2 = input('Enter interface type and number: ')
+i3 = input(dict[i1]['Question'])
+dict[i1]['Interface'] = i2
+dict[i1]['Vlan'] = i3
+print((dict[i1]['Template'].format(dict[i1]['Interface'], dict[i1]['Vlan'])))
