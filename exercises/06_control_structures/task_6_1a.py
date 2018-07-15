047ff9197eaa777d707b868c1e2b0b8fe0c32c3c
jewdas1984 <alexatwork0@gmail.com>
2018-07-15 03:33:16 -0400

    Solved

--- exercises/06_control_structures/task_6_1a.py
+++ exercises/06_control_structures/task_6_1a.py
@@ -16,3 +16,29 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+ip_address = input('Введите IP адрес в формате 10.0.1.1: ')
+
+octet_A = ip_address.split('.')[0]
+octet_B = ip_address.split('.')[1]
+octet_C = ip_address.split('.')[2]
+octet_D = ip_address.split('.')[3]
+
+if 0 <= int(octet_A) <= 255 and 0 <= int(octet_B) <= 255 and 0 <= int(octet_C) <= 255 and 0 <= int(octet_D) <= 255:
+        print('Адрес верного формата')
+else:
+        print('Ваш адрес неверен')
+
+if '255.255.255.255' in ip_address:
+        print('Ваш адрес LOCAL BROADCAST')
+elif '0.0.0.0' in ip_address:
+        print('Ваш адрес UNASSIGNED')    
+elif int(octet_A) <= 127:
+        print('Ваш адрес UNICAST класса A')
+elif int(octet_A) > 127 and int(octet_A) <= 191:
+        print('Ваш адрес UNICAST класса B')
+elif int(octet_A) > 192 and int(octet_A) <= 223:
+        print('Ваш адрес UNICAST класса C')
+elif int(octet_A) > 223 and int(octet_A) <= 239:
+        print('Ваш адрес MULTICAST класса D')
+else:
+     print('Ваш адрес UNUSED')
