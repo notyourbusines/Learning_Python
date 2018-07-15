3db8f6e340342b17a4a5fb7d1b7f7807dc934179
jewdas1984 <alexatwork0@gmail.com>
2018-07-15 03:49:52 -0400

    Solved

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -14,3 +14,10 @@
 mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
 
 mac_cisco = []
+converted = '0'
+for mac_address in mac:
+    converted = str(mac_address).replace(':', '.')
+    mac_cisco.append(converted)
+    
+print(mac_cisco)
+
