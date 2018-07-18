f41be7cbf4ff740c8ee7d0d4f1d9a3a762f23682
jewdas1984 <alexatwork0@gmail.com>
2018-07-18 07:31:59 -0400

    Solved

--- exercises/07_files/task_7_3a.py
+++ exercises/07_files/task_7_3a.py
@@ -11,3 +11,25 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+mac_template = '''{}      {}          {}'''
+
+f = open('CAM_table.txt', 'r')
+vlan_raw = []
+
+for lines in f:
+    lines = lines.split()
+    if 'DYNAMIC' not in lines:
+        continue
+    else:
+        vlan_raw.append(lines)
+        #vlan, mac, learn, ports = lines
+        #print(mac_template.format(vlan, mac, ports))
+
+result = sorted(vlan_raw, key=lambda x: x[0])
+
+for zaloopa in result:
+    a,b,c,d = zaloopa
+    print(mac_template.format(a, b, c, d))
+
+f.close()
+
--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -11,3 +11,27 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+mac_template = '''{}      {}          {}'''
+
+in1 = input('Введите номер VLAN: ')
+
+f = open('CAM_table.txt', 'r')
+vlan_raw = []
+
+for lines in f:
+    lines = lines.split()
+    if 'DYNAMIC' not in lines:
+        continue
+    else:
+        vlan_raw.append(lines)
+
+result = sorted(vlan_raw, key=lambda x: x[0])
+
+for zaloopa in result:
+    a,b,c,d = zaloopa
+    if in1 not in a:
+        continue
+    else:
+        print(mac_template.format(a, b, c, d))
+
+f.close()
