cb73507cd2c4b7e051f0db45bb76167816f9a2cc
jewdas1984 <alexatwork0@gmail.com>
2018-07-21 05:02:57 -0400

    Solved

--- exercises/09_functions/task_9_1.py
+++ exercises/09_functions/task_9_1.py
@@ -38,28 +38,28 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
-
-
 def generate_access_config(access):
-    '''
-    access - словарь access-портов,
-    для которых необходимо сгенерировать конфигурацию, вида:
-        { 'FastEthernet0/12':10,
-          'FastEthernet0/14':11,
-          'FastEthernet0/16':17}
-
-    Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
-    '''
-    access_template = [
-        'switchport mode access', 'switchport access vlan',
-        'switchport nonegotiate', 'spanning-tree portfast',
-        'spanning-tree bpduguard enable'
-    ]
-
-
-access_dict = {
-    'FastEthernet0/12': 10,
-    'FastEthernet0/14': 11,
-    'FastEthernet0/16': 17,
-    'FastEthernet0/17': 150
-}
+2	    access_template = [
+3	        'switchport mode access', 'switchport access vlan',
+4	        'switchport nonegotiate', 'spanning-tree portfast',
+5	        'spanning-tree bpduguard enable'
+6	    ]
+7	    
+8	    for intf in access:
+9	        print('interface ' + intf)
+10	        for each in access_template:
+11	            if each.endswith('access vlan'):
+12	                print(each + ' ' + str(access[intf]))
+13	            else: 
+14	                print(each)
+15	                
+16	
+17	
+18	access_dict = {
+19	    'FastEthernet0/12': 10,
+20	    'FastEthernet0/14': 11,
+21	    'FastEthernet0/16': 17,
+22	    'FastEthernet0/17': 150
+23	}
+24	
+25	generate_access_config(access_dict)
