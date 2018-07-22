e424110f30a810979de72b50fa1c1afd12f138c5
jewdas1984 <alexatwork0@gmail.com>
2018-07-22 07:02:20 -0400

    Solved

--- exercises/09_functions/task_9_3a.py
+++ exercises/09_functions/task_9_3a.py
@@ -23,3 +23,41 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+def get_int_vlan_map(config_file):
+    
+    f = open(config_file, 'r')
+    
+    sort_lst = []
+    tmp_sort_list =()
+    for each in f:
+        if 'interface FastEthernet' in each or 'access vlan' in each or 'allowed vlan' in each or 'mode access' in each:
+            each = each.strip("\n")
+            each = each.strip(" ")
+            sort_lst.append(each)
+    
+    print(sort_lst)
+    
+    access_dict = {}
+    trunk_dict = {}
+
+    for i in range(len(sort_lst)):
+        if sort_lst[i].startswith('interface'):
+            if (i+2) > len(sort_lst)-1:
+                print('Программа завершена!')
+                break
+            elif sort_lst[i+1].endswith('mode access'):
+                if sort_lst[i+2].startswith('switchport access'):
+                    access_dict[sort_lst[i].split()[1]] = sort_lst[i+2].split()[3]
+                    print(access_dict)
+                else:
+                    access_dict[sort_lst[i].split()[1]] = '1'
+                    print(access_dict)
+            elif sort_lst[i+1].startswith('switchport trunk'):
+                trunk_dict[sort_lst[i].split()[1]] = sort_lst[i+1].split()[4]
+                print(trunk_dict)
+            else:
+                print('Интерфейс без конфига!')
+    
+    
+get_int_vlan_map('config_sw2.txt')
