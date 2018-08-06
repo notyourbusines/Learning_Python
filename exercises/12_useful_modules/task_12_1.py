91cc38cc3aad2f36e05767266e922f1dd86ed779
jewdas1984 <alexatwork0@gmail.com>
2018-08-06 13:28:01 -0400

    Solved

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,26 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+import subprocess
+
+def check_ip_addresses(ip_list):
+    reachable_list = []
+    unreachable_list = []
+    
+    for each in ip_list:
+        result = subprocess.run(['ping', '-c', '3', '-n', each])
+        if result.returncode == 0:
+            reachable_list.append(each)
+        else:
+            unreachable_list.append(each)
+            
+    print(reachable_list)
+    print(unreachable_list)
+
+
+list_of_ips = ('8.8.8.8', '192.1.1.1')
+#list_of_ips = ('8.8.8.8')
+
+check_ip_addresses(list_of_ips)
+    
