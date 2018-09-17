fe9f28390ed551c0c2122567eb8ca25d32d8b7bd
python <python@localhost.localdomain>
2018-09-17 08:03:03 -0400

    Solved

--- exercises/17_serialization/task_17_2a.py
+++ exercises/17_serialization/task_17_2a.py
@@ -25,3 +25,42 @@
 
 Не копировать код функции parse_sh_cdp_neighbors
 '''
+
+import re
+import glob
+import yaml
+
+def parse_sh_cdp_neighbors(output):
+    rslt = {}
+    for each in output:
+        tmp = {}
+        l_hname_parse = re.search('(\w+)(\>)', each)
+        cdp_parse = re.search('(?P<r_hname>\S+)\s+(?P<l_intf>\S+\s+\d+/\d+).+(?P<r_intf>Eth \d+/\d+)', each)
+        if l_hname_parse:
+            l_hname = l_hname_parse.group(0)
+            rslt[l_hname] = {}
+        elif cdp_parse:
+            r_hname = cdp_parse.group('r_hname')
+            #print(r_hname)
+            l_intf = cdp_parse.group('l_intf')
+            #print(l_intf)
+            r_intf = cdp_parse.group('r_intf')
+            #print(r_intf)
+            tmp[r_hname] = r_intf
+            rslt[l_hname][l_intf] = tmp
+    return(rslt)
+
+sh_version_files = glob.glob('sh_cdp_n_*')
+
+zaloopa = []
+
+for filez in sh_version_files:
+    with open(filez) as f:
+        pizda = f.readlines()
+        xyz = parse_sh_cdp_neighbors(pizda)
+        zaloopa.append(xyz)
+
+with open('topology.yaml', 'w') as xyz:
+    yaml.dump(zaloopa, xyz)
+
+
