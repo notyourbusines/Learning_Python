23a2717177dc995531964012b7e494257ec357a7
python <python@localhost.localdomain>
2018-09-20 15:01:37 -0400

    Solved

--- exercises/18_db/task_18_2a/task_18_2a.py
+++ exercises/18_db/task_18_2a/task_18_2a.py
@@ -18,3 +18,47 @@ $ python get_data_ver1.py vln 10
 Допустимые значения параметров: mac, ip, vlan, interface, switch
 
 '''
+
+import sqlite3
+from sys import argv
+from tabulate import tabulate
+
+def connect_to_db():
+    db_filename = 'dhcp_snooping.db'
+    connection = sqlite3.connect(db_filename)
+    return connection
+
+if argv[1] not in {'mac', 'ip', 'vlan', 'interface', 'switch'}:
+    print('This data pattern is not supported! ')
+elif(len(argv) == 3):
+    keyz, valuez = argv[1:]
+    piska = connect_to_db()
+    cursor = piska.cursor()
+    cursor.execute('SELECT * from dhcp WHERE {} = "{}"'.format(keyz, valuez))
+    rowz = cursor.fetchall()
+    rslt = []
+    tmp = []
+    for each in rowz:
+        #print(each)
+        #tmp = []
+        mac, vlan, _, intf, sw = each
+        mac_mac = ['mac', mac]
+        vlan_vlan = ['ip', vlan]
+        intf_intf = ['interface', intf]
+        sw_sw = ['sw', sw]
+        tmp.append(tuple(mac_mac))
+        tmp.append(tuple(vlan_vlan))
+        tmp.append(tuple(intf_intf))
+        tmp.append(tuple(sw_sw))
+        print(tmp)
+    print(tabulate(tmp))
+elif (len(argv) == 1):
+    piska = connect_to_db()
+    cursor = piska.cursor()
+    cursor.execute('SELECT * from dhcp')
+    rowz = cursor.fetchall()
+    print('Table DHCP contains following records: ')
+    print((rowz))
+    #print(tabulate(rowz))
+else:
+    print('User 0 or 2 arguments only!')
