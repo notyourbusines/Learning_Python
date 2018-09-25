908407da51cdeb5414f4555b9769c40ae0e3c23a
python <python@localhost.localdomain>
2018-09-25 12:38:49 -0400

    Solved

--- exercises/19_ssh_telnet/task_19_1a.py
+++ exercises/19_ssh_telnet/task_19_1a.py
@@ -10,3 +10,31 @@
 
 Для проверки измените пароль на устройстве или в файле devices.yaml.
 '''
+
+import yaml
+import netmiko
+from netmiko import ConnectHandler
+
+command = 'sh ip int br'
+
+def send_show_command(dev_to_send, commands_to_send):
+    for each in dev_to_send:
+        try:
+            with ConnectHandler(**each) as ssh:
+                ssh.enable()
+        
+                result = ssh.send_command(commands_to_send)
+                print(result)
+                
+        except netmiko.ssh_exception.NetMikoAuthenticationException:
+                #incorrect credentials
+                print('Authentication failed!')
+                continue
+
+with open('devices.yaml') as dev:
+        devs_param = yaml.load(dev)
+        for zzz in devs_param['routers']:
+            #print(zzz)
+            p = devs_param['routers']
+        
+send_show_command(p, command)
