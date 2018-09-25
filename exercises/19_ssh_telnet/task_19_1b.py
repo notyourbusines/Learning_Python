c9c292f9b6c38e19d7738050d1ba180528da6941
python <python@localhost.localdomain>
2018-09-25 12:55:23 -0400

    Solved

--- exercises/19_ssh_telnet/task_19_1b.py
+++ exercises/19_ssh_telnet/task_19_1b.py
@@ -11,3 +11,35 @@
 
 Для проверки измените IP-адрес на устройстве или в файле devices.yaml.
 '''
+import yaml
+import netmiko
+from netmiko import ConnectHandler
+
+command = 'sh ip int br'
+
+def send_show_command(dev_to_send, commands_to_send):
+    for each in dev_to_send:
+        #print(each)
+        try:
+            with ConnectHandler(**each) as ssh:
+                ssh.enable()
+        
+                result = ssh.send_command(commands_to_send)
+                print(result)
+                
+        except netmiko.ssh_exception.NetMikoAuthenticationException:
+            #incorrect credentials
+            print('Authentication failed!')
+            continue
+        except netmiko.ssh_exception.NetMikoTimeoutException:
+            #wrong IP address
+            print('Connection Timeout for {} host!'.format(each['ip']))
+            continue
+
+with open('devices.yaml') as dev:
+        devs_param = yaml.load(dev)
+        for zzz in devs_param['routers']:
+            #print(zzz)
+            p = devs_param['routers']
+        
+send_show_command(p, command)
