478d624455479ab189b9aa7c30945a0c60d883a3
python <python@localhost.localdomain>
2018-10-01 14:39:30 -0400

    Solved

--- exercises/19_ssh_telnet/task_19_2d.py
+++ exercises/19_ssh_telnet/task_19_2d.py
@@ -35,3 +35,59 @@ commands_with_errors = ['logging 0255.255.1', 'logging', 'i']
 correct_commands = ['logging buffered 20010', 'ip http server']
 
 commands = commands_with_errors + correct_commands
+
+error_msg = 'Incomplete command', 'Ambiguous command', 'Invalid input detected at'
+
+failed_comms = {}
+passed_comms = {}
+rslt_fld = {}
+rslt_pssd = {}
+rslt_overall = []
+import netmiko
+import yaml
+from netmiko import ConnectHandler
+
+def send_config_command(dev_to_send, commands_to_send):
+    for each in dev_to_send:
+        IP_add = str(each['ip'])
+        try:
+            with ConnectHandler(**each) as ssh:
+                ssh.enable()
+                flag = False
+                for pizda in commands_to_send:
+                    if flag == True:
+                        break
+                    result = ssh.send_config_set(pizda)
+                    for piska in error_msg:
+                        if piska in result:
+                            failed_comms[pizda] = result
+                            rslt_fld['failed_comms_{}'.format(IP_add)] = failed_comms
+                            print('Обнаружена ошибка {} при выполнении команды {}'.format(piska, pizda))
+                            print('Выполнять эту команду для других устройств? [Y]/n')
+                            inpt = input()
+                            if inpt == 'Y':
+                                break
+                            else:
+                                commands_to_send.remove(pizda)
+                                #flag = True
+                                break
+                    else:
+                        passed_comms[pizda] = result
+                        rslt_pssd['passed_comms_{}'.format(IP_add)] = passed_comms
+        except netmiko.ssh_exception.NetMikoAuthenticationException:
+            #incorrect credentials
+            print('Authentication failed!')
+            continue
+        except netmiko.ssh_exception.NetMikoTimeoutException:
+            #wrong IP address
+            print('Connection Timeout for {} host!'.format(each['ip']))
+            continue
+    rslt_overall = [rslt_pssd, rslt_fld]
+    print(tuple(rslt_overall))
+
+with open('devices.yaml') as dev:
+        devs_param = yaml.load(dev)
+        #print(devs_param['routers'])
+        p = devs_param['routers']
+        
+zopa = send_config_command(p, commands)
