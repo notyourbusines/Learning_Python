44102dc6298f995a18c36c3951f97816fc7b1ff9
python <python@localhost.localdomain>
2018-10-03 08:40:43 -0400

    Solved

--- exercises/19_ssh_telnet/task_19_3.py
+++ exercises/19_ssh_telnet/task_19_3.py
@@ -31,7 +31,95 @@
 
 '''
 
+import netmiko
+import yaml
+from netmiko import ConnectHandler
+
 commands = [
     'logging 10.255.255.1', 'logging buffered 20010', 'no logging console'
 ]
 command = 'sh ip int br'
+
+def send_commands(device, show=False, filename=False, config=False):
+    if show:
+        zaloopec = send_show_command(p, command)
+        print(zaloopec)
+    elif filename:
+        zaloopec = send_from_filename(p, 'config.txt')
+        print(zaloopec)
+    elif config:
+        zaloopec = send_config_command(p, commands)
+        print(zaloopec)
+    
+
+
+with open('devices.yaml') as dev:
+    devs_param = yaml.load(dev)
+    for zzz in devs_param['routers']:
+        #print(zzz)
+        p = devs_param['routers']
+            
+def send_show_command(dev_to_send, commands_to_send):
+    rslt = {}
+    for each in dev_to_send:
+        #print(each)
+        try:
+            with ConnectHandler(**each) as ssh:
+                ssh.enable()
+                result = ssh.send_command(commands_to_send)
+                tmp_ip = each['ip']
+                rslt[tmp_ip] = result
+        except netmiko.ssh_exception.NetMikoAuthenticationException:
+            #incorrect credentials
+            print('Authentication failed!')
+            continue
+        except netmiko.ssh_exception.NetMikoTimeoutException:
+            #wrong IP address
+            print('Connection Timeout for {} host!'.format(each['ip']))
+            continue
+    return rslt
+            
+def send_config_command(dev_to_send, commands_to_send):
+    rslt = {}
+    for each in dev_to_send:
+        try:
+            with ConnectHandler(**each) as ssh:
+                ssh.enable()
+                for pizda in commands_to_send:
+                    result = ssh.send_config_set(pizda)
+                    tmp_ip = each['ip']
+                    rslt[tmp_ip] = result
+                    
+        except netmiko.ssh_exception.NetMikoAuthenticationException:
+            #incorrect credentials
+            print('Authentication failed!')
+            continue
+        except netmiko.ssh_exception.NetMikoTimeoutException:
+            #wrong IP address
+            print('Connection Timeout for {} host!'.format(each['ip']))
+            continue
+    return rslt
+
+def send_from_filename(dev_to_send, filenm):
+    rslt = {}
+    for each in dev_to_send:
+        with open(filenm) as f:
+            try:
+                with ConnectHandler(**each) as ssh:
+                    ssh.enable()
+                    #print(each)
+                    for pepiska in f:
+                        result = ssh.send_config_set(pepiska)
+                        tmp_ip = each['ip']
+                        rslt[tmp_ip] = result
+            except netmiko.ssh_exception.NetMikoAuthenticationException:
+                #incorrect credentials
+                print('Authentication failed!')
+                continue
+            except netmiko.ssh_exception.NetMikoTimeoutException:
+                #wrong IP address
+                print('Connection Timeout for {} host!'.format(each['ip']))
+                continue
+    return rslt
+
+send_commands(p, show=True)
