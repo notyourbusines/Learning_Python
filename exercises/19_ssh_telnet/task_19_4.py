7c64c2192a01cbc4afc4f97025aa244d74b8f7c8
python <python@localhost.localdomain>
2018-10-04 14:48:58 -0400

    Solved

--- exercises/19_ssh_telnet/task_19_4.py
+++ exercises/19_ssh_telnet/task_19_4.py
@@ -18,3 +18,44 @@
 Функция send_commands_to_devices должна использовать функцию send_commands из задания 19.3.
 
 '''
+
+import netmiko
+import yaml
+from netmiko import ConnectHandler
+import getpass
+from task_19_3 import send_commands
+
+with open('devices2.yaml') as dev:
+    devs_param = yaml.load(dev)
+    p = devs_param['routers']
+
+def check_ping(device_to_check):
+    reply = subprocess.run(['ping', '-c', '3', '-n', device_to_check])
+    if reply.returncode == False:
+        return True
+    else:
+        return False
+
+def send_commands_to_devices(devices_list, show=False, filename=False, config=False):
+    resulting_list = []
+    uname = input('Please input username: ')
+    print('Please input password: ')
+    password = getpass.getpass()
+    print('Please input enable password: ')
+    enable_pwd = getpass.getpass()
+    for each in devices_list:
+        each['username'] = uname
+        each['password'] = password
+        each['secret'] = enable_pwd
+        resulting_list.append(each)
+    if show:
+        print('Running SHOW command on devices: ')
+        send_commands(resulting_list, show=True)
+    elif filename:
+        print('Running commands from file on devices: ')
+        send_commands(resulting_list, filename=True)
+    elif config:
+        print('Running CONFIG command on devices: ')
+        send_commands(resulting_list, config=True)
+
+send_commands_to_devices(p, show=True)
--- exercises/19_ssh_telnet/task_19_4a.py
+++ exercises/19_ssh_telnet/task_19_4a.py
@@ -15,3 +15,53 @@
 и затем использовать ее в функции send_commands_to_devices.
 
 '''
+import netmiko
+import yaml
+from netmiko import ConnectHandler
+import getpass
+from task_19_3 import send_commands
+import subprocess
+
+with open('devices2.yaml') as dev:
+    devs_param = yaml.load(dev)
+    p = devs_param['routers']
+
+def check_ping(device_to_check):
+    reply = subprocess.run(['ping', '-c', '3', '-n', device_to_check])
+    if reply.returncode == False:
+        return True
+    else:
+        return False
+
+def send_commands_to_devices(devices_list, show=False, filename=False, config=False):
+    resulting_list = []
+    last_list = []
+    uname = input('Please input username: ')
+    print('Please input password: ')
+    password = getpass.getpass()
+    print('Please input enable password: ')
+    enable_pwd = getpass.getpass()
+    for each in devices_list:
+        each['username'] = uname
+        each['password'] = password
+        each['secret'] = enable_pwd
+        resulting_list.append(each)
+    for xyz in resulting_list:
+        print('Текущий элемент списка: ', xyz)
+        chpok = check_ping(xyz['ip'])
+        if not chpok:
+            continue
+        else:
+            last_list.append(xyz)
+    #print(last_list)
+    if show:
+        #print('Running SHOW command on devices: ')
+        send_commands(last_list, show=True)
+    elif filename:
+        print('Running commands from file on devices: ')
+        send_commands(last_list, filename=True)
+    elif config:
+        print('Running CONFIG command on devices: ')
+        send_commands(last_list, config=True)
+
+send_commands_to_devices(p, show=True)
