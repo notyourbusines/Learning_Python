c84797c3691f4167caac7742a0af1074b74ff53c
python <python@localhost.localdomain>
2018-10-09 12:55:53 -0400

    Solved

--- exercises/21_jinja2/task_21_1.py
+++ exercises/21_jinja2/task_21_1.py
@@ -14,21 +14,36 @@
 
 '''
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Template
 import yaml
 import sys
 import os
 
 #$ python cfg_gen.py templates/for.txt data_files/for.yml
-TEMPLATE_DIR, template = os.path.split(sys.argv[1])
-VARS_FILE = sys.argv[2]
+#TEMPLATE_DIR, template = os.path.split(sys.argv[1])
+#VARS_FILE = sys.argv[2]
 
-env = Environment(
-    loader=FileSystemLoader(TEMPLATE_DIR),
-    trim_blocks=True,
-    lstrip_blocks=True)
-template = env.get_template(template_file)
+#env = Environment(
+#    loader=FileSystemLoader(TEMPLATE_DIR),
+#    trim_blocks=True,
+#    lstrip_blocks=True)
+#template = env.get_template(template)
 
-vars_dict = yaml.load(open(VARS_FILE))
+#vars_dict = yaml.load(open(VARS_FILE))
 
-print(template.render(vars_dict))
+#print(template.render(vars_dict))
+
+
+
+def generate_cfg_from_template(template_dir, varibls):
+    
+    tmp_dir, tmplate = os.path.split(template_dir)
+       
+    env = Environment(loader=FileSystemLoader(tmp_dir))
+    template = env.get_template(tmplate)
+    
+    variables = yaml.load(open(varibls))
+    
+    print(template.render(variables).strip('\n'))
+    
+generate_cfg_from_template('templates/for.txt', 'data_files/for.yml')
